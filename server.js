cefd367b2c6180acb856947fd2bf07e607361ea5
Mario Flores Jr <m.flores4821@gmail.com>
2018-03-06 14:57:25 -0800

    server/query work

--- server.js
+++ server.js
@@ -30,13 +30,16 @@ app.get('/', (req, res) => {
 
   let query = '&filter[esrb][eq]=2,3';
   //add query values from homepage form
-  if(req.query.age) query += `&filter[esrb][eq]=${req.query.age}`;
-  if(req.query.genre) query += `&filter[genre][eq]=${req.query.genre}`;
+  if(req.query.name) query += `&filter[name][eq]=${req.query.name}`;
+  if(req.query.age) query += `&filter[esrb.rating][eq]=${req.query.age}`;
+  if(req.query.genres) query += `&filter[genres][eq]=${req.query.genres}`;
   if(req.query.console) query += `&filter[isbn][eq]=${req.query.isbn}`;
   if(req.query.ratings) query += `&filter[total_rating_count][lt]=${req.query.ratings}`;
   if(req.query.score && req.query.score < 50) query += `&filter[rating][gte]=${req.query.score}`;
   if(req.query.score && req.query.score > 50) query += `&filter[rating][lte]=${req.query.score}`;
   if(req.query.yearRange) query += `&filter[release_date.date][in]=${req.query.yearRange}`;
+  if(req.query.first_release_date) query += `&filter[first_release_date][in]=${req.query.first_release_date}`;
+  if(req.query.platforms) query += `&filter[platforms][in]=${req.query.platforms}`;
 
   //set URL for API query
   let url = `https://api-2445582011268.apicast.io/games/?fields=*${query}&limit=25&offset=0`;
@@ -47,15 +50,15 @@ app.get('/', (req, res) => {
     .set({'user-key': API_KEY})
     .then(res => console.log(res.body))//need to build from here
     .catch(console.error);
-})
+});
 
 // let query = '&filter[rating][gte]=75&filter[genres][eq]=5';
 // let url = `https://api-2445582011268.apicast.io/games/?fields=name,genres,platforms,esrb.rating,first_release_date${query}&limit=25`;
 //   console.log (url); This one works don't touch.
 
 let query = '&filter[first_release_date][lt]=1104537600000&filter[rating][gte]=75&filter[genres][eq]=5';
-let url = `https://api-2445582011268.apicast.io/games/?fields=name,genres,platforms,esrb.rating,first_release_date${query}&limit=25`;
-  console.log (url);
+let url = `https://api-2445582011268.apicast.io/games/?fields=name,genres,platforms,esrb.rating,first_release_date,esrb${query}&limit=25`;
+console.log (url);
 
 // let query = '&filter[first_release_date][gt]=1500619813000&filter[rating][gte]=75&filter[genres][eq]=5';
 // let url = `https://api-2445582011268.apicast.io/games/?fields=name,genres,platforms,esrb.rating,first_release_date${query}&limit=25`;
@@ -64,15 +67,38 @@ let url = `https://api-2445582011268.apicast.io/games/?fields=name,genres,platfo
 // let query = '&filter[rating][gte]=75&filter[genres][eq]=5';
 // let url = `https://api-2445582011268.apicast.io/release_dates/?fields=*&filter[platform][eq]=48&order=date:asc&filter[date][gt]=1500619813000&expand=game`;
 
-//   console.log (url); 
-  //  this works but the let url appears to adjust the date the games are filtered by, not the let query.
+//   console.log (url);
+//  this works but the let url appears to adjust the date the games are filtered by, not the let query.
 
 // let query = '&count?filter[release_dates.platform][eq]=72';
 // let url = `https://api-2445582011268.apicast.io/games/?fields=name,genres,platforms,esrb.rating,release_dates${query}&limit=25`;
 //   console.log (url);   trying to figur out filterin by date test 1.
 
-  //get request from API
+//get request from API
+superagent.get(url)
+  .set({'user-key': API_KEY})
+  .then(res => console.log(res.body))//need to build from here
+  .catch(console.error);
+
+  let { name, genres, platforms, esrb.rating, first_release_date } = game.volumeInfo;
+
+  let placeholderImage = 
+  'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
+
+  return {
+    name: name ? name : 'No title available',
+    genres: genres ? genres[0] : 'No genres available',
+    platforms: platforms ? platforms : 'No platforms available',
+    // esrb.rating: esrb.rating ? esrb.rating : 'No rating available',
+    first_release_date: first_release_date ? first_release_date : 'No date available',
+  }
+}))
+.then(game => res.send(game[0]))
+.catch(console.error)
+})
+
+app.get('https://api-2445582011268.apicast.io/games', (req, res) => {
+  let url = 'https://api-2445582011268.apicast.io/games/?fields=*${query}&limit=25&offset=0';
   superagent.get(url)
-    .set({'user-key': API_KEY})
-    .then(res => console.log(res.body))//need to build from here
-    .catch(console.error);
+  .set({'user-key': API_KEY})
+}
