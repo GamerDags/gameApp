f1e9ddfc6600a058d079147b56fa69d9108fa667
Mario Flores Jr <m.flores4821@gmail.com>
2018-03-08 14:59:36 -0800

    about us work

--- public/scripts/controller/games.js
+++ public/scripts/controller/games.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app = app || {};
-var __API_URL__ = 'http://localhost:3000';
+var __API_URL__ = 'https://gamerdags.herokuapp.com/';
 
 (function(module) {
   function errorCallback(err) {
